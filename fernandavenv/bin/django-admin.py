dbc6147a3931c7af08343122b4e7ce8d4d3bf567
maikonsx <maikonsx@gmail.com>
2018-05-19 22:00:54 -0300

    EmpurrandoOnline

---
+++ fernandavenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/maiconfuck/Documentos/ParaFernanda/Fernanda/fernandavenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
