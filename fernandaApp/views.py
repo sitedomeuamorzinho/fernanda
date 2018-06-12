357bbf833f52e9179cd130cfe6387a9eac3881b8
sitedomeuamorzinho <maiconfotosbackup@gmail.com>
2018-06-11 22:08:05 -0300

    Nova Pagina

--- fernandaApp/views.py
+++ fernandaApp/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, "fernandaApp/index.html")
+
+def namorados(request):
+	return render(request, "fernandaApp/namorados.html")
